652d534a78da1ae06c5a736ff64fd2804ea2c41f
Robot Assisprex <Robot@DESKTOP-LCTQ4V4.domassisprex.local>
2022-02-17 16:13:14 -0500

    Metodos para ver el total de ventas y buscar  por descripcion

--- src/co/edu/unbosque/model/TransferCsvToCompra.java
+++ src/co/edu/unbosque/model/TransferCsvToCompra.java
@@ -1,44 +1,80 @@
 package co.edu.unbosque.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 import co.edu.unbosque.model.csv.ReadCSV;
 
 public class TransferCsvToCompra {
 
-	ArrayList <Compra> listaCompras = new ArrayList<>();
-	private ReadCSV rc;
+    private ArrayList<Compra> listaCompras = new ArrayList<>();
+    private final ReadCSV rc;
 
-	public TransferCsvToCompra() {
-		rc = new ReadCSV();
-	}
+    public TransferCsvToCompra() {
+        rc = new ReadCSV();
+    }
 
-	public String mostrarresultados() {
-		String respuesta = "";
-		listaCompras = rc.ReadFromPath("csv/data.csv");
-		respuesta = listaCompras.get(541908).toString();
+
+    // punto 2
+
+    public double totalVentas() {
+        double total = 0.0;
+        for (Compra compra : listaCompras) {
+            double aux = compra.getQuantity() * compra.getUnitPrice();
+            total += aux; // total = total + aux;
+        }
+        return total;
+    }
+
+    public ArrayList<Compra> buscarPorDescripcion(String search, boolean order, int initMonth, int endMonth) {
+        ArrayList<Compra> comprasEncontradas = new ArrayList<>();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Calendar calendar = new GregorianCalendar();
+
+        try {
+            for (Compra compra : listaCompras) {
+                String descripcionActual = compra.getDescription();
+                Date date = simpleDateFormat.parse(compra.getInvoiceDate());
+                calendar.setTime(date);
+                int mes = calendar.get(Calendar.MONTH);
+
+                if (descripcionActual.contains(search)) {
+                    if (mes >= initMonth && mes <= endMonth) {
+                        comprasEncontradas.add(compra);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return comprasEncontradas;
+    }
+
+    public String mostrarresultados() {
+        String respuesta = "";
+        listaCompras = rc.ReadFromPath("csv/data.csv");
+        respuesta = listaCompras.get(541908).toString();
 //		for (int i = 0; i < listaCompras.size(); i++) {
 //			respuesta= listaCompras.get(1).toString();
 //		}
-		return respuesta;
-	}
-	
-	public ArrayList <Compra> agregarCompra(Compra c) {
-		listaCompras.add(c);
-		return listaCompras;
-		
-	}
+        return respuesta;
+    }
 
-	public ArrayList<Compra> getListaCompras() {
-		return listaCompras;
-	}
+    public ArrayList<Compra> agregarCompra(Compra c) {
+        listaCompras.add(c);
+        return listaCompras;
 
-	public void setListaCompras(ArrayList<Compra> listaCompras) {
-		this.listaCompras = listaCompras;
-	}
+    }
 
-	
+    public ArrayList<Compra> getListaCompras() {
+        return listaCompras;
+    }
+
+    public void setListaCompras(ArrayList<Compra> listaCompras) {
+        this.listaCompras = listaCompras;
+    }
+
 
 //	public List<Compra> getCsvlist() {
 //		if (csvlist != null) return csvlist;
@@ -48,7 +84,6 @@ public class TransferCsvToCompra {
 //	public void setCsvlist(List<Compra> csvlist) {
 //		this.csvlist = csvlist;
 //	}
-
 
 
 }
